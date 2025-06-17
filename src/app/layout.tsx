6868d6a78f54ec8cf195b55b52a0a4b9aee2a28d
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2025-06-17 07:08:48 +0000

    use this logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6699CC" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#3C6E71" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
