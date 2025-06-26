8cea74bd8e7167e0c8231ee30bd1231cc38fcab8
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2025-06-26 05:27:03 +0000

    Installation not available

--- src/next.config.ts
+++ src/next.config.ts
@@ -6,7 +6,6 @@ const pwaConfig = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
     fallbacks: {
         document: '/offline',
     }
