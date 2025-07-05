399c2b2516c0c250222ba0e5e69e9a2201d15a69
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2025-07-05 05:02:35 +0000

    still exists

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,20 @@
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  self.clients.claim();
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    self.registration.unregister()
+      .then(() => {
+        return self.clients.matchAll({ type: 'window' });
+      })
+      .then((clients) => {
+        clients.forEach(client => {
+            if (client.url && 'navigate' in client) {
+                client.navigate(client.url);
+            }
+        });
+      })
+  );
 });
