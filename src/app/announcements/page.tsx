2cde645fc8ac5a4edc628b4479a2bbd4eb392dde
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2025-07-15 05:45:38 +0000

    why can't I select the date today?

--- src/app/announcements/page.tsx
+++ src/app/announcements/page.tsx
@@ -224,7 +224,7 @@ export default function AnnouncementsPage() {
                                 </FormControl>
                             </PopoverTrigger>
                             <PopoverContent className="w-auto p-0" align="start">
-                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date()} initialFocus />
+                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date(new Date().setDate(new Date().getDate() - 1))} initialFocus />
                             </PopoverContent>
                             </Popover>
                             <FormMessage />
@@ -237,7 +237,7 @@ export default function AnnouncementsPage() {
                         render={({ field }) => (
                         <FormItem>
                             <FormLabel>Time</FormLabel>
-                            <FormControl><Input type="time" {...field} /></FormControl>
+                            <FormControl><Input type="time" {...field} value={field.value || ''} /></FormControl>
                             <FormMessage />
                         </FormItem>
                         )}
