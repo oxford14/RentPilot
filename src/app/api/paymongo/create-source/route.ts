2f280cf9317f2e22ddfb4db990ef8869ded2006c
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2026-01-20 02:59:58 +0000

    QR Code URL not found in PayMongo response.

--- src/app/api/paymongo/create-source/route.ts
+++ src/app/api/paymongo/create-source/route.ts
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
@@ -46,7 +48,7 @@ export async function POST(request: Request) {
         data: {
           attributes: {
             amount: Math.round(amount * 100), // Amount in centavos
-            currency: 'PHP', // Add the required currency field
+            currency: 'PHP',
             payment_method_allowed: ['qrph'],
             payment_method_options: {
               qrph: {
