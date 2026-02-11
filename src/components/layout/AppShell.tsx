f3d7b541ed39d3c48e578c765e972b8667f82022
Neil Andrew Fuerzas <neilandrewfuerzas@gmail.com>
2026-02-11 01:07:22 +0000

    Try fixing this error: `Runtime Error: Error: FileViewIcon is not define

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -833,7 +833,7 @@ function AppShellContent({ children }: { children: ReactNode }) {
                 {authUser?.role === 'tenant' && currentTenant?.signedContractUrl && (
                   <DropdownMenuItem asChild>
                     <a href={currentTenant.signedContractUrl} target="_blank" rel="noopener noreferrer">
-                      <FileViewIcon className="mr-2 h-4 w-4" />
+                      <FileText className="mr-2 h-4 w-4" />
                       <span>View My Contract</span>
                     </a>
                   </DropdownMenuItem>
@@ -895,3 +895,5 @@ export function AppShell({ children }: { children: ReactNode }) {
         </SidebarProvider>
     )
 }
+
+    
